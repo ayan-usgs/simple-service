c5aa7235f76362f3ca3c1c3bc3d9bbda45b83725
Andrew Yan <ayan@usgs.gov>
2018-09-20 12:50:15 -0500

    update behavior if no environment variables are provided

--- my_app/environment_getter.py
+++ my_app/environment_getter.py
@@ -7,7 +7,11 @@ import falcon
 class EnvironmentGetter:
 
     def on_get(self, req, resp):
-        env_var = req.params['variable']
-        env_var_result = os.getenv(env_var)
-        resp.body = json.dumps({env_var: env_var_result})
+        env_var = req.params.get('variable')
+        if env_var is None:
+            env_vars = os.environ.keys()
+            resp.body = json.dumps({'variables': len(env_vars)})
+        else:
+            env_var_result = os.getenv(env_var)
+            resp.body = json.dumps({env_var: env_var_result})
         resp.status = falcon.HTTP_200
