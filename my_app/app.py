07632bfacac5aa62187200669c494e10023cc1e9
Andrew Yan <ayan@usgs.gov>
2018-09-21 15:07:50 -0500

    change env getter url

--- my_app/app.py
+++ my_app/app.py
@@ -9,6 +9,6 @@ eg = EnvironmentGetter()
 eo = EvenOdd()
 sl = StringLength()
 
-api.add_route('/env_getter/', eg)
+api.add_route('/env-getter/', eg)
 api.add_route('/integer/', eo)
 api.add_route('/string-length/', sl)
